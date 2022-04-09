c753cc5a2e4833916b7bfdb340921f3bee4050bb
Murilo Santos <mucrilobass@gmail.com>
2022-04-09 11:58:20 -0400

    Frag2, Spinner e sons criados.

--- app/src/main/java/com/example/trabalho1/SecondFragment.java
+++ app/src/main/java/com/example/trabalho1/SecondFragment.java
@@ -1,35 +1,91 @@
 package com.example.trabalho1;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.trabalho1.databinding.FragmentSecondBinding;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.media.MediaPlayer;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SecondFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private Spinner spinner;
+    private MediaPlayer mPlayer;
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        }
+    }
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_second, container, false);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.sons, android.R.layout.simple_spinner_item);
+
+        spinner=view.findViewById(R.id.spinner_sec_frag_sound);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                if(mPlayer!=null){
+                    mPlayer.release();
+                }
+                switch (pos){
+                    case 1:
+                        mPlayer=MediaPlayer.create(getContext(),R.raw.cachorro);
+                        break;
+                    case 2:
+                        mPlayer=MediaPlayer.create(getContext(),R.raw.cavalo);
+                        break;
+                    case 3:
+                        mPlayer=MediaPlayer.create(getContext(),R.raw.galo);
+                        break;
+                    case 4:
+                        mPlayer=MediaPlayer.create(getContext(),R.raw.gato);
+                        break;
+                    case 5:
+                        mPlayer=MediaPlayer.create(getContext(),R.raw.macaco);
+                        break;
+                    case 6:
+                        mPlayer=MediaPlayer.create(getContext(),R.raw.vaca);
+                        break;
+                }
+                if(mPlayer!=null){
+                    mPlayer.start();
+                    mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            mediaPlayer.release();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecondToFirst.setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_second_to_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
@@ -37,11 +93,4 @@ public class SecondFragment extends Fragment {
             }
         });
     }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
 }
